940abcbb50049d6f1e1f28d40faaa8d0f4bdc720
Trigdev <ahorisaac.ai@gmail.com>
2022-09-19 13:43:23 +0100

    App.tsx file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
