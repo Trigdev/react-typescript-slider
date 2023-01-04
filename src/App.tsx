ea9897d20926a5fd3dd09fd52318fdc24014da51
Trigdev <ahorisaac.ai@gmail.com>
2023-01-04 09:19:31 +0100

    v1.0.0 Finalized ~ 🚀

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, FC, ReactElement } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 import { DataProps } from "./App.types";
 
-const App = () => {
+const App: FC = (): ReactElement => {
   const [people, setPeople] = useState<Array<DataProps>>(data);
   const [index, setIndex] = useState<number>(0);
 
@@ -56,7 +56,7 @@ const App = () => {
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
-              <p className="text">{quote}</p>
+              <p className="text color-black">{quote}</p>
               <FaQuoteRight className="icon" />
             </article>
           );
